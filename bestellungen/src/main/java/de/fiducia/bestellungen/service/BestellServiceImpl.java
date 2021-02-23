8edec8b5ff8c02592a40e0a5e91bb30c280c4566
limagoHub <limagoHub@limago.de>
2021-02-23 16:12:41 +0100

    Birgit

--- bestellungen/src/main/java/de/fiducia/bestellungen/service/BestellServiceImpl.java
+++ bestellungen/src/main/java/de/fiducia/bestellungen/service/BestellServiceImpl.java
@@ -1,28 +1,84 @@
 package de.fiducia.bestellungen.service;
 
+import java.rmi.RemoteException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import de.fiducia.bestellungen.models.Bestellung;
 import de.fiducia.bestellungen.repositories.BestellRepository;
 
-public class BestellServiceImpl {
-	
-	private final BestellRepository bestellRepository;
-	private final SolvenzService solvenzService;
-	
-	public BestellServiceImpl(BestellRepository bestellRepository, SolvenzService solvenzService) {
-		
-		this.bestellRepository = bestellRepository;
-		this.solvenzService = solvenzService;
-	}
-	// Creditkarte = Entweder M oder V direkt gefolgt von exakt 10 ziffern, also 11 Stellen
-	/*
-	 * Nullprüfung -> BestellException
-	 * Format der Karte ist falsch -> BestellException
-	 * Wenn einer der Services nicht erreichbar -> BestellException
-	 * Wenn pleite PleiteException
-	 * Erst Solvenzprüfung, dann Bestellung speichern
-	 */
-	public void bestellungErfassen(Bestellung bestellung, String creditkarte, double saldo) throws BestellServiceException, KundeIstPleitException{
-		
-	}
+public class BestellServiceImpl
+{
+
+    private static final String PATTERN_KREDITKARTE = "^(M|V)(\\d{10})$";
+    private final BestellRepository bestellRepository;
+    private final SolvenzService solvenzService;
+
+    public BestellServiceImpl(BestellRepository bestellRepository, SolvenzService solvenzService)
+    {
+
+        this.bestellRepository = bestellRepository;
+        this.solvenzService = solvenzService;
+    }
+    // Creditkarte = Entweder M oder V direkt gefolgt von exakt 10 ziffern, also 11 Stellen
+    /*
+     * Nullprüfung -> BestellException DONE
+     * Format der Karte ist falsch -> BestellException DONE
+     * Wenn einer der Services nicht erreichbar -> BestellException
+     * Wenn pleite PleiteException
+     * Erst Solvenzprüfung, dann Bestellung speichern
+     * Saldo muss >= 0 sein -> BestellException
+     */
+    public void bestellungErfassen(Bestellung bestellung, String creditkarte, double saldo)
+        throws BestellServiceException, KundeIstPleitException
+    {
+
+        try
+        {
+            bestellungErfassenImpl(bestellung, creditkarte, saldo);
+        }
+
+        catch (RemoteException e)
+        {
+            throw new BestellServiceException("SolvenzService nicht verfügbar.", e);
+        }
+        catch (RuntimeException e)
+        {
+            throw new BestellServiceException("Repo nicht verfügbar.", e);
+        }
+    }
+    private void bestellungErfassenImpl(Bestellung bestellung, String creditkarte, double saldo)
+        throws BestellServiceException, RemoteException, KundeIstPleitException
+    {
+        final Pattern pattern = Pattern.compile(PATTERN_KREDITKARTE);
+
+        if (null == bestellung)
+        {
+            throw new BestellServiceException("Bestellung darf nicht null sein.");
+        }
+        if (null == creditkarte)
+        {
+            throw new BestellServiceException("Kreditkarte darf nicht null sein.");
+        }
+        if (saldo < 0)
+        {
+            throw new BestellServiceException("Saldo darf nicht kleiner 0 sein.");
+        }
+
+
+        final Matcher matcher = pattern.matcher(creditkarte);
+        if (!matcher.matches())
+        {
+            throw new BestellServiceException(
+                "Kreditkartenname muss mit M oder V beginnen, danach genau 10 Ziffern enthalten.");
+        }
+        final String type = matcher.group(1).equals("M") ? "Master" : "Visa";
+        final String number = matcher.group(2);
+        if (!solvenzService.check(type, number, saldo))
+        {
+            throw new KundeIstPleitException("Kunde ist pleite");
+        }
+        bestellRepository.save(bestellung);
+    }
 
 }
--- bestellungen/src/test/java/de/fiducia/bestellungen/service/BestellServiceImplTest.java
+++ bestellungen/src/test/java/de/fiducia/bestellungen/service/BestellServiceImplTest.java
@@ -1,20 +1,172 @@
 package de.fiducia.bestellungen.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.rmi.RemoteException;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import de.fiducia.bestellungen.models.Bestellung;
 import de.fiducia.bestellungen.repositories.BestellRepository;
 
 @ExtendWith(MockitoExtension.class)
-public class BestellServiceImplTest {
-	@Mock
-	private BestellRepository bestellRepository;
+public class BestellServiceImplTest
+{
+    private static final String INVALID_KREDITKARTE_BUCHSTABE_IM_ZIFFERNBEREICH = "M123456789A";
 
-	@Mock
-	private SolvenzService solvenzService;
-	@InjectMocks
-	private BestellServiceImpl bestellServiceImpl;
+    private static final String INVALID_KREDITKARTE_FALSCHER_ANFANGSBUCHSTABE = "S1234567890";
+
+    private static final String EXCEPTION_MESSAGE_FALSCHES_FORMAT_KREDITKARTE = "Kreditkartenname muss mit M oder V beginnen, danach genau 10 Ziffern enthalten.";
+
+    private static final String INVALID_KREDITKARTE_ZU_LANG = "M12345678901";
+
+    private static final String INVALID_KREDITKARTE_ZU_KURZ = "M123456789";
+
+    private static final double INVALID_SALDO = -1.5;
+
+    private static final Bestellung VALID_BESTELLUNG = new Bestellung();
+
+    private static final double VALID_SALDO = 10.0;
+
+    private static final String VALID_MASTERCARD = "M1234567890";
+
+    @Mock
+    private BestellRepository bestellRepositoryMock;
+
+    @Mock
+    private SolvenzService solvenzServiceMock;
+
+    @InjectMocks
+    private BestellServiceImpl serviceUnderTest;
+
+
+    @Test
+    void bestellungErfassen_mitBestellungNull_throwsBestellException()
+    {
+        BestellServiceException ex = assertThrows(BestellServiceException.class,
+            () -> serviceUnderTest.bestellungErfassen(null, VALID_MASTERCARD, VALID_SALDO));
+        assertEquals("Bestellung darf nicht null sein.", ex.getMessage());
+    }
+
+    @Test
+    void bestellungErfassen_mitKreditkarteNull_throwsBestellException()
+    {
+        BestellServiceException ex = assertThrows(BestellServiceException.class,
+            () -> serviceUnderTest.bestellungErfassen(VALID_BESTELLUNG, null, VALID_SALDO));
+        assertEquals("Kreditkarte darf nicht null sein.", ex.getMessage());
+    }
+
+    @Test
+    void bestellungErfassen_mitSaldoNegativ_throwsBestellException()
+    {
+        BestellServiceException ex = assertThrows(BestellServiceException.class,
+            () -> serviceUnderTest.bestellungErfassen(VALID_BESTELLUNG, VALID_MASTERCARD, INVALID_SALDO));
+        assertEquals("Saldo darf nicht kleiner 0 sein.", ex.getMessage());
+    }
+
+    @Test
+    void bestellungErfassen_mitKreditkarteLaengeKleinerElf_throwsBestellException()
+    {
+        BestellServiceException ex = assertThrows(BestellServiceException.class,
+            () -> serviceUnderTest.bestellungErfassen(VALID_BESTELLUNG, INVALID_KREDITKARTE_ZU_KURZ, VALID_SALDO));
+        assertEquals(EXCEPTION_MESSAGE_FALSCHES_FORMAT_KREDITKARTE, ex.getMessage());
+    }
+
+    @Test
+    void bestellungErfassen_mitKreditkarteLaengeZuLang_throwsBestellException()
+    {
+        BestellServiceException ex = assertThrows(BestellServiceException.class,
+            () -> serviceUnderTest.bestellungErfassen(VALID_BESTELLUNG, INVALID_KREDITKARTE_ZU_LANG, VALID_SALDO));
+        assertEquals(EXCEPTION_MESSAGE_FALSCHES_FORMAT_KREDITKARTE, ex.getMessage());
+    }
+
+    @Test
+    void bestellungErfassen_mitKreditkarteFalscherAnfangsbuchstabe_throwsBestellException()
+    {
+        BestellServiceException ex = assertThrows(BestellServiceException.class,
+            () -> serviceUnderTest.bestellungErfassen(VALID_BESTELLUNG, INVALID_KREDITKARTE_FALSCHER_ANFANGSBUCHSTABE,
+                VALID_SALDO));
+        assertEquals(EXCEPTION_MESSAGE_FALSCHES_FORMAT_KREDITKARTE, ex.getMessage());
+    }
+
+    @Test
+    void bestellungErfassen_mitKreditkarteBuchstabeInZiffernbereich_throwsBestellException()
+    {
+        BestellServiceException ex = assertThrows(BestellServiceException.class,
+            () -> serviceUnderTest.bestellungErfassen(VALID_BESTELLUNG, INVALID_KREDITKARTE_BUCHSTABE_IM_ZIFFERNBEREICH,
+                VALID_SALDO));
+        assertEquals(EXCEPTION_MESSAGE_FALSCHES_FORMAT_KREDITKARTE, ex.getMessage());
+    }
+
+    @Test
+    void bestellungErfassen_solvenzServiceNichtErreichbar_throwsBestellException() throws Exception
+    {
+        doThrow(new RemoteException("Service nicht verfügbar.")).when(solvenzServiceMock).check(anyString(),
+            anyString(), anyDouble());
+        BestellServiceException ex = assertThrows(BestellServiceException.class,
+            () -> serviceUnderTest.bestellungErfassen(VALID_BESTELLUNG, VALID_MASTERCARD, VALID_SALDO));
+        assertEquals("SolvenzService nicht verfügbar.", ex.getMessage());
+    }
+
+    @Test
+    void bestellungErfassen_solvenzServiceWithValidParameters_parametersPassedToService() throws Exception
+    {
+        when(solvenzServiceMock.check(anyString(), anyString(), anyDouble())).thenReturn(true);
+
+        serviceUnderTest.bestellungErfassen(VALID_BESTELLUNG, VALID_MASTERCARD, VALID_SALDO);
+
+        verify(solvenzServiceMock).check("Master", "1234567890", VALID_SALDO);
+    }
+
+    @Test
+    void bestellungErfassen_kundeIstPleite_throwsKundeIstPleiteException() throws Exception
+    {
+        when(solvenzServiceMock.check(anyString(), anyString(), anyDouble())).thenReturn(false);
+
+        KundeIstPleitException ex = assertThrows(KundeIstPleitException.class,
+            () -> serviceUnderTest.bestellungErfassen(VALID_BESTELLUNG, VALID_MASTERCARD, VALID_SALDO));
+
+        assertEquals("Kunde ist pleite", ex.getMessage());
+    }
+
+    @Test
+    void bestellungErfassen_repoNichtVerfuegbar_throwsBestellException() throws Exception
+    {
+        doThrow(new RuntimeException()).when(bestellRepositoryMock).save(any());
+        when(solvenzServiceMock.check(anyString(), anyString(), anyDouble())).thenReturn(true);
+
+        BestellServiceException ex = assertThrows(BestellServiceException.class,
+            () -> serviceUnderTest.bestellungErfassen(VALID_BESTELLUNG, VALID_MASTERCARD, VALID_SALDO));
+
+        assertEquals("Repo nicht verfügbar.", ex.getMessage());
+
+    }
+
+    @Test
+    void bestellungErfassen_korrekteParameter_BestellungWirdAnRepoUebergeben() throws Exception
+    {
+        when(solvenzServiceMock.check(anyString(), anyString(), anyDouble())).thenReturn(true);
+
+        serviceUnderTest.bestellungErfassen(VALID_BESTELLUNG, VALID_MASTERCARD, VALID_SALDO);
+
+        InOrder inOrder = Mockito.inOrder(solvenzServiceMock, bestellRepositoryMock);
+        inOrder.verify(solvenzServiceMock, times(1)).check("Master", "1234567890", VALID_SALDO);
+        inOrder.verify(bestellRepositoryMock, times(1)).save(VALID_BESTELLUNG);
+
+    }
 
 }
