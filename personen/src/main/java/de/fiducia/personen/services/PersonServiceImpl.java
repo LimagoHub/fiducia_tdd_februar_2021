d90295c01db81c69782f599c2a6a15e6504c560e
limagoHub <limagoHub@limago.de>
2021-02-23 11:48:52 +0100

    Fertiger Test

--- personen/src/main/java/de/fiducia/personen/services/PersonServiceImpl.java
+++ personen/src/main/java/de/fiducia/personen/services/PersonServiceImpl.java
@@ -1,23 +1,56 @@
 package de.fiducia.personen.services;
 
+import java.util.List;
+
 import de.fiducia.personen.PersonRepositoriy;
 import de.fiducia.personen.models.Person;
 
 public class PersonServiceImpl {
 
 	private final PersonRepositoriy repository;
+	private final List<String> antipathen;
 
-	public PersonServiceImpl(final PersonRepositoriy repository) {
+	public PersonServiceImpl(final PersonRepositoriy repository, final List<String> antipathen) {
 		this.repository = repository;
+		this.antipathen = antipathen;
 	}
 	
-	// 1. Parameter darf nicht null sein. -> PersonServiceException
-	// 2. Vorname darf nicht null und nicht weniger als 2 enthalten  -> PersonServiceException
-	// 3. Nachname darf nicht null und nicht weniger als 2 enthalten  -> PersonServiceException
-	// 4. Vorname darf nicht Attila sein  -> PersonServiceException
+	// 1. Parameter darf nicht null sein. -> PersonServiceException ##Häkchen
+	// 2. Vorname darf nicht null und nicht weniger als 2 enthalten  -> PersonServiceException ##Häkchen
+	// 3. Nachname darf nicht null und nicht weniger als 2 enthalten  -> PersonServiceException ##Häkchen
+	// 4. Vorname darf nicht Attila sein  -> PersonServiceException ##Häkchen
 	// 5. Exception im Repo -> PersonServiceException
 	// guter Fall. Person im Repo speichern
 	public void speichern(Person person) throws PersonServiceException{
+		try {
+			speichernImpl(person);
+		} catch (RuntimeException e) {
+			throw new PersonServiceException("Fehler im Service.");
+		}
 		
+	}
+
+	private void speichernImpl(Person person) throws PersonServiceException {
+		checkPerson(person);
+		repository.save(person);
+	}
+
+	private void checkPerson(Person person) throws PersonServiceException {
+		validatedPerson(person);
+		businessCheck(person);
+	}
+
+	private void businessCheck(Person person) throws PersonServiceException {
+		if(antipathen.contains(person.getVorname())) 
+			throw new PersonServiceException("Antipath");
+	}
+
+	private void validatedPerson(Person person) throws PersonServiceException {
+		if(person == null) 
+			throw new PersonServiceException("Person darf nicht null sein.");
+		if(person.getVorname() == null || person.getVorname().length() < 2) 
+			throw new PersonServiceException("Vorname muss min 2 Zeichen enthalten.");
+		if(person.getNachname() == null || person.getNachname().length() < 2) 
+			throw new PersonServiceException("Nachname muss min 2 Zeichen enthalten.");
 	}
 }
--- personen/src/test/java/de/fiducia/personen/services/PersonServiceImplTest.java
+++ personen/src/test/java/de/fiducia/personen/services/PersonServiceImplTest.java
@@ -1,21 +1,35 @@
 package de.fiducia.personen.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import de.fiducia.personen.PersonRepositoriy;
+import de.fiducia.personen.models.Person;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonServiceImplTest {
 	
+	private static final String VALID_FIRTSTNAME = "John";
+	private static final String VALID_LASTNAME = "Doe";
+	private static final String VALID_ID = UUID.randomUUID().toString();
 	@Mock
 	private PersonRepositoriy repositoryMock;
+	
+	@Mock
+	private List<String> antipathenMock;
+	
 	@InjectMocks
 	private PersonServiceImpl objectUnderTest;
 	
@@ -26,5 +40,99 @@ public class PersonServiceImplTest {
 		assertEquals("Person darf nicht null sein.", ex.getMessage());
 	
 	}
+
+	@Test
+	void speichern_VornameNull_throwsPersonServiceException() throws Exception{
+		
+		Person person = Person.builder().id(VALID_ID).vorname(null).nachname(VALID_LASTNAME).build();
+		
+		PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(person));
+		assertEquals("Vorname muss min 2 Zeichen enthalten.", ex.getMessage());
+	
+	}
+
+	@Test
+	void speichern_VornameZuKurz_throwsPersonServiceException() throws Exception{
+		
+		Person person = Person.builder().id(VALID_ID).vorname("J").nachname(VALID_LASTNAME).build();
+		
+		PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(person));
+		assertEquals("Vorname muss min 2 Zeichen enthalten.", ex.getMessage());
+	
+	}
+
+	@Test
+	void speichern_NachnameNull_throwsPersonServiceException() throws Exception{
+		
+		Person person = Person.builder().id(VALID_ID).vorname(VALID_FIRTSTNAME).nachname(null).build();
+		
+		PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(person));
+		assertEquals("Nachname muss min 2 Zeichen enthalten.", ex.getMessage());
+	
+	}
+
+	@Test
+	void speichern_NachnameZuKurz_throwsPersonServiceException() throws Exception{
+		
+		Person person = Person.builder().id(VALID_ID).vorname(VALID_FIRTSTNAME).nachname("D").build();
+		
+		PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(person));
+		assertEquals("Nachname muss min 2 Zeichen enthalten.", ex.getMessage());
+	
+	}
+
+	@Test
+	void speichern_Antipath_throwsPersonServiceException() throws Exception{
+		
+		Person person = Person.builder().id(VALID_ID).vorname(VALID_FIRTSTNAME).nachname("Doe").build();
+		
+		when(antipathenMock.contains(anyString())).thenReturn(true);
+		
+		PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(person));
+		assertEquals("Antipath", ex.getMessage());
+	
+	}
+
+	@Test
+	void speichern_RuntimeExceptionInUnderlyingService_throwsPersonServiceException() throws Exception{
+		
+		Person person = Person.builder().id(VALID_ID).vorname(VALID_FIRTSTNAME).nachname(VALID_LASTNAME).build();
+		
+		doThrow(new ArrayIndexOutOfBoundsException()).when(repositoryMock).save(any());
+		
+		PersonServiceException ex = assertThrows(PersonServiceException.class, ()->objectUnderTest.speichern(person));
+		assertEquals("Fehler im Service.", ex.getMessage());
+	
+	}
+
+	@Test
+	void speichern_ParameterPassedToUnderlyingService_ParameterReceived() throws Exception{
+		
+		final Person person = Person.builder().id(VALID_ID).vorname(VALID_FIRTSTNAME).nachname(VALID_LASTNAME).build();
+		
+		//doThrow(new ArrayIndexOutOfBoundsException()).when(repositoryMock).save(any());
+		when(antipathenMock.contains(anyString())).thenReturn(false);
+		
+		objectUnderTest.speichern(person);
+		verify(repositoryMock, times(1)).save(person);
+	
+	}
+
+	@Test
+	void speichern_HappyDay_PersonSavedInRepository() throws Exception{
+		
+		final Person person = Person.builder().id(VALID_ID).vorname(VALID_FIRTSTNAME).nachname(VALID_LASTNAME).build();
+		
+		//doThrow(new ArrayIndexOutOfBoundsException()).when(repositoryMock).save(any());
+		when(antipathenMock.contains(anyString())).thenReturn(false);
+		objectUnderTest.speichern(person);
+		
+		// Fachliche Rege wird überprüft (Kein Algorithmus)
+		InOrder inOrder = inOrder(antipathenMock, repositoryMock);
+		inOrder.verify(antipathenMock, times(1)).contains(VALID_FIRTSTNAME);
+		inOrder.verify(repositoryMock, times(1)).save(person);
+	
+	}
+
 
 }
